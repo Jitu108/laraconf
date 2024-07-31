38b8b329aa606954e233443b36bfbc6df5a1f0c6
Jitendra Gupta <jitendra@Shashis-MacBook-Air.local>
2024-08-01 04:33:31 +0530

    Worked on Conference UI

--- app/Filament/Resources/ConferenceResource.php
+++ app/Filament/Resources/ConferenceResource.php
@@ -28,63 +28,7 @@ class ConferenceResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->label(label: 'Conference')
-                    ->required()
-                    ->default(state: 'My Conference')
-                    ->hint(hint: "The name of the Conference")
-                    ->hintIcon(icon: 'heroicon-o-information-circle')
-                    ->maxLength(100),
-
-                Forms\Components\MarkdownEditor::make('description')
-                    //->disableToolbarButtons()
-                    //->toolbarButtons(buttons: ['h2', 'bold'])
-                    ->required(),
-
-                Forms\Components\DateTimePicker::make('start_date')
-                    ->native(condition: false)
-                    ->helperText(text: "Please include the time when the conference will start along with Date")
-                    ->required(),
-
-                Forms\Components\DateTimePicker::make('end_date')
-                    ->native(condition: false)
-                    ->helperText(text: "Please include the tentative time when the Conference will end")
-                    ->required(),
-
-                Forms\Components\Toggle::make(name: 'is_published')
-                    ->default(state: true),
-
-                Forms\Components\Select::make('status')
-                    ->options(options: [
-                        'draft' => 'Draft',
-                        'published' => 'Published',
-                        'archieved' => 'Archieved'
-                    ])
-                    ->required(),
-
-                Forms\Components\Select::make(name: 'region')
-                    ->live()
-                    ->required()
-                    ->enum(enum: Region::class)
-                    ->options(options: Region::class),
-
-                Forms\Components\Select::make('venue_id')
-                    ->searchable()
-                    ->preload()
-                    ->editOptionForm(Venue::getForm())
-                    ->createOptionForm(Venue::getForm())
-                    ->relationship('venue', 'name', modifyQueryUsing: function (Builder $query, Forms\Get $get) {
-                        return $query->where(column: 'region', operator: $get('region'));
-                    }),
-
-                // Forms\Components\CheckboxList::make('speakers')
-                //     ->relationship(name: 'speakers', titleAttribute: 'name')
-                //     ->options(
-                //         options: Speaker::all()->pluck('name', 'id')
-                //     )
-                //     ->searchable()
-            ]);
+            ->schema(Conference::getForm());
     }
 
     public static function table(Table $table): Table
--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -2,8 +2,18 @@
 
 namespace App\Models;
 
+use Filament\Forms\Get;
 use App\Enums\Region;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Toggle;
 use Illuminate\Database\Eloquent\Model;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Tabs;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -33,5 +43,154 @@ class Conference extends Model
     public function talks(): BelongsToMany
     {
         return $this->belongsToMany(Talk::class);
+    }
+
+    public static function getForm(): array
+    {
+        return [
+
+            // Tabs::make()
+            //     ->columnSpanFull()
+            //     ->tabs([
+            //         Tabs\Tab::make('Conference Details')
+            //             ->schema([
+            //                 TextInput::make('name')
+            //                     ->columnSpanFull()
+            //                     ->label(label: 'Conference')
+            //                     ->required()
+            //                     ->default(state: 'My Conference')
+            //                     ->hint(hint: "The name of the Conference")
+            //                     ->hintIcon(icon: 'heroicon-o-information-circle')
+            //                     ->maxLength(100),
+
+            //                 MarkdownEditor::make('description')
+            //                     ->columnSpanFull()
+            //                     //->disableToolbarButtons()
+            //                     //->toolbarButtons(buttons: ['h2', 'bold'])
+            //                     ->required(),
+
+            //                 DateTimePicker::make('start_date')
+            //                     ->native(condition: false)
+            //                     ->helperText(text: "Please include the time when the conference will start along with Date")
+            //                     ->required(),
+
+            //                 DateTimePicker::make('end_date')
+            //                     ->native(condition: false)
+            //                     ->helperText(text: "Please include the tentative time when the Conference will end")
+            //                     ->required(),
+
+            //                 Fieldset::make('Status')
+            //                     ->columns(1)
+            //                     ->schema([
+
+            //                         Select::make('status')
+            //                             ->options(options: [
+            //                                 'draft' => 'Draft',
+            //                                 'published' => 'Published',
+            //                                 'archieved' => 'Archieved'
+            //                             ])
+            //                             ->required(),
+            //                         Toggle::make(name: 'is_published')
+            //                             ->default(state: true),
+
+            //                     ])
+            //             ]),
+            //         Tabs\Tab::make('Location')
+            //             ->schema([
+            //                 Select::make(name: 'region')
+            //                     ->live()
+            //                     ->required()
+            //                     ->enum(enum: Region::class)
+            //                     ->options(options: Region::class),
+
+            //                 Select::make('venue_id')
+            //                     ->searchable()
+            //                     ->preload()
+            //                     ->editOptionForm(Venue::getForm())
+            //                     ->createOptionForm(Venue::getForm())
+            //                     ->relationship('venue', 'name', modifyQueryUsing: function (Builder $query, Get $get) {
+            //                         return $query->where(column: 'region', operator: $get('region'));
+            //                     }),
+            //             ])
+
+            //     ]),
+
+            Section::make(heading: 'Conference Details')
+                ->collapsible()
+                ->columns(2)
+                ->schema([
+                    TextInput::make('name')
+                        ->columnSpanFull()
+                        ->label(label: 'Conference')
+                        ->required()
+                        ->default(state: 'My Conference')
+                        ->hint(hint: "The name of the Conference")
+                        ->hintIcon(icon: 'heroicon-o-information-circle')
+                        ->maxLength(100),
+
+                    MarkdownEditor::make('description')
+                        ->columnSpanFull()
+                        //->disableToolbarButtons()
+                        //->toolbarButtons(buttons: ['h2', 'bold'])
+                        ->required(),
+
+                    DateTimePicker::make('start_date')
+                        ->native(condition: false)
+                        ->helperText(text: "Please include the time when the conference will start along with Date")
+                        ->required(),
+
+                    DateTimePicker::make('end_date')
+                        ->native(condition: false)
+                        ->helperText(text: "Please include the tentative time when the Conference will end")
+                        ->required(),
+
+                    Fieldset::make('Status')
+                        ->columns(1)
+                        ->schema([
+
+                            Select::make('status')
+                                ->options(options: [
+                                    'draft' => 'Draft',
+                                    'published' => 'Published',
+                                    'archieved' => 'Archieved'
+                                ])
+                                ->required(),
+                            Toggle::make(name: 'is_published')
+                                ->default(state: true),
+
+                        ])
+                ]),
+
+            Section::make(heading: 'Location')
+                ->columns(2)
+                ->schema([
+                    Select::make(name: 'region')
+                        ->live()
+                        ->required()
+                        ->enum(enum: Region::class)
+                        ->options(options: Region::class),
+
+                    Select::make('venue_id')
+                        ->searchable()
+                        ->preload()
+                        ->editOptionForm(Venue::getForm())
+                        ->createOptionForm(Venue::getForm())
+                        ->relationship('venue', 'name', modifyQueryUsing: function (Builder $query, Get $get) {
+                            return $query->where(column: 'region', operator: $get('region'));
+                        }),
+                ]),
+
+
+
+
+
+
+            // CheckboxList::make('speakers')
+            //     ->relationship(name: 'speakers', titleAttribute: 'name')
+            //     ->options(
+            //         options: Speaker::all()->pluck('name', 'id')
+            //     )
+            //     ->searchable()
+        ];
     }
 }
